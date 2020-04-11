65ae61627b8d19efc9e3a7e5aeea77c5b0baa555
Ger Hobbelt <ger@hobbelt.com>
2020-04-11 18:41:46 +0200

    fix in test code for `this.slug()` filter usage in the `permalink` function: there, you don't have a `this.slug`.

--- test/stubs/class-data-permalink-fn-filter.11ty.js
+++ test/stubs/class-data-permalink-fn-filter.11ty.js
@@ -2,8 +2,20 @@ class Test {
   get data() {
     return {
       title: "My Super Cool Title",
-      permalink: function({ title }) {
-        return `/my-permalink/${this.slug(title)}/`;
+      permalink: function({ title }, config, tmpl) {
+        console.error("TEST PERMALINK FN:",{cfg: config.slug, tpl: tmpl.slug})
+        function myGetFilter(name) {
+          return (
+            config.javascriptFunctions[name] ||
+            config.nunjucksFilters[name] ||
+            config.liquidFilters[name] ||
+            config.handlebarsHelpers[name]
+          );
+        }
+
+        let slugFn = myGetFilter("slug");
+        //return `/my-permalink/${this.slug(title)}/`;
+        return `/my-permalink/${slugFn(title)}/`;
       }
     };
   }
